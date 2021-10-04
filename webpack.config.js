0cf5b154dc10c56200763f798505a5fdfc6219c1
Dmitriy <schajckodmitrij@gmail.com>
2021-10-04 23:40:42 +0300

    firstSteps_creatingHTML-and-CSS

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/js/index.js',
+  module: {
+    rules: [
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.(js)$/, use: 'babel-loader' }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+  },
+  mode: 'development', 
+  watch: true
+}
